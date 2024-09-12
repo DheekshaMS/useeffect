9543eea1d4e4202611bae899d3ddcd4b2d70610b
DheekshaMS <147329953+DheekshaMS@users.noreply.github.com>
2024-09-12 10:41:11 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+
+function Timer() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount(count + 1);
+    }, 1000);
+
+    return () => clearInterval(interval); // Cleanup on unmount
+  }, [count]);
+  return <h1>{count}</h1>;
+}
